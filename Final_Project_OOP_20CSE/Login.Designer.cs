b8628d49eb1b095ce2c636f76377653465c4afa4
ngobao2002 <bao.ngo200202@vnuk.edu.vn>
2021-12-14 17:55:51 +0700

    Update

--- Final_Project_OOP_20CSE/Login.Designer.cs
+++ Final_Project_OOP_20CSE/Login.Designer.cs
@@ -61,7 +61,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(566, 28);
+            this.pictureBox2.Location = new System.Drawing.Point(590, 28);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(216, 73);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
